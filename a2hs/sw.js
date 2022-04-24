9af8bead2a972fe512a2f7d5ccb2ea066090ba1d
Florian Morin <fmorin@et.esiea.fr>
2022-04-24 12:30:15 +0200

    fix install ?

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/',
-      '/pwa-examples/index.html',
-      '/pwa-examples/index.js',
-      '/pwa-examples/style.css',
-      '/pwa-examples/images/fox1.jpg',
-      '/pwa-examples/images/fox2.jpg',
-      '/pwa-examples/images/fox3.jpg',
-      '/pwa-examples/images/fox4.jpg',
+      '/pwa-examples/a2hs/',
+      '/pwa-examples/a2hs/index.html',
+      '/pwa-examples/a2hs/index.js',
+      '/pwa-examples/a2hs/style.css',
+      '/pwa-examples/a2hs/images/fox1.jpg',
+      '/pwa-examples/a2hs/images/fox2.jpg',
+      '/pwa-examples/a2hs/images/fox3.jpg',
+      '/pwa-examples/a2hs/images/fox4.jpg',
     ])),
   );
 });
